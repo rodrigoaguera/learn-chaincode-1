a610fcbb3d7a14e6c56ba4cd35a4f70a5fedc84e
Rodrigo Aguera <rodrigoaguera@uol.com.br>
2016-12-14 14:09:17 -0200

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -69,7 +69,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 	// Handle different functions
 	if function == "read" { //read a variable
-		var msg = "Estou de ferias. Volto logo!"		
+		var msg = "Hoje e o terceiro dia de treinamento"		
 		return []byte(msg), nil 		
 		//return t.read(stub, args)
 	}
